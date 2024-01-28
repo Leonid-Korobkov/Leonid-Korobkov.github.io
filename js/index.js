f78e092ea7551d028f286081d966fec1790f900d
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-01-28 16:22:17 +0300

    optimize images + skeleton

--- js/index.js
+++ js/index.js
@@ -3,5 +3,16 @@ import './lazyload.min.js'
 const lazyMedia = new LazyLoad({
   elements_selector: '[data-src],[data-srcset]',
   class_loaded: '_lazy-loaded',
-  use_native: true
-});
+  use_native: true,
+  callback_loaded: el => {
+    const imgContainer = el.closest('.main__img-container')
+    // imgContainer.style.aspectRatio = 'auto';
+    if (imgContainer) {
+    imgContainer.style.height = 'auto';
+      const skeleton = imgContainer.querySelector('.skeleton')
+      if (skeleton) {
+        imgContainer.removeChild(skeleton)
+      }
+    }
+  }
+})
