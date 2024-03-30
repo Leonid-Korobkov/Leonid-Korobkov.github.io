0a144a12897ee71d32bfbd5c81282109a0faea3c
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-03-30 13:32:30 +0300

    update structure files + add data.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
 import './lazyload.min.js'
+import { projectsData } from './projectsData.js'
+
+function renderProjects() {
+  const $projectList = document.querySelector('.main__list')
+
+  let stringToInsert = ``
+  projectsData.forEach(item => {
+    const projectItem = `
+        <a class="main__item-link" href="${item.link}">
+          <li class="main__item">
+            <span class="main__link">${item.title}</span>
+            <div class="main__img-container">
+              <div class="skeleton"></div>
+              <img class="main__img" data-src="${item.imageSrc}"
+                alt="${item.title}">
+            </div>
+          </li>
+        </a>`
+
+    stringToInsert += projectItem
+  })
+
+  $projectList.innerHTML = stringToInsert
+}
+
+renderProjects()
 
 const lazyMedia = new LazyLoad({
   elements_selector: '[data-src],[data-srcset]',
@@ -7,7 +33,7 @@ const lazyMedia = new LazyLoad({
   callback_loaded: el => {
     const imgContainer = el.closest('.main__img-container')
     if (imgContainer) {
-      imgContainer.style.aspectRatio = 'auto';
+      imgContainer.style.aspectRatio = 'auto'
       const skeleton = imgContainer.querySelector('.skeleton')
       if (skeleton) {
         imgContainer.removeChild(skeleton)
---
+++ js/projectsData.js
@@ -0,0 +1,152 @@
+export const projectsData = [
+  {
+    title: 'Веб-приложение для заметок (KRB Notes)',
+    link: 'https://krb-notes.vercel.app/',
+    imageSrc: 'img/portfolio/min/krb-notes.jpg',
+    alt: 'Веб-приложение для заметок (KRB Notes)'
+  },
+  {
+    title: 'Перевод систем счисления (math-project)',
+    link: 'https://leonid-korobkov.github.io/math-project/',
+    imageSrc: 'img/portfolio/min/math-project.jpg',
+    alt: 'Перевод систем счисления (math-project)'
+  },
+  {
+    title: 'Верстка сайта под Wordpress. НОЭКС - Строительная компания',
+    link: 'http://o92141m5.beget.tech/noex/',
+    imageSrc: 'img/portfolio/min/noex.jpg',
+    alt: 'Верстка сайта под Wordpress. НОЭКС - Строительная компания'
+  },
+  {
+    title: 'Real-time Chat. React, Firebase, MUI',
+    link: 'https://chat-react-krbln.vercel.app/',
+    imageSrc: 'img/portfolio/min/realtime-chat.jpg',
+    alt: 'Real-time Chat. React, Firebase, MUI'
+  },
+  {
+    title: 'Мини-клон Excel. HTML, CSS, JS.',
+    link: 'https://excel-krbln.web.app/',
+    imageSrc: 'img/portfolio/min/excel.jpg',
+    alt: 'Мини-клон Excel. HTML, CSS, JS.'
+  },
+  {
+    title: 'Простое веб-приложение для заметок (simple-notes)',
+    link: 'https://simple-notes-react.vercel.app/',
+    imageSrc: 'img/portfolio/min/simple-notes.png',
+    alt: 'Простое веб-приложение для заметок (simple-notes)'
+  },
+  {
+    title: 'Таймер для медитации. HTML, CSS, JS.',
+    link: 'https://leonid-korobkov.github.io/timer-meditation/',
+    imageSrc: 'img/portfolio/min/timer-meditation.jpg',
+    alt: 'Таймер для медитации. HTML, CSS, JS.'
+  },
+  {
+    title: 'Мини-конструктор сайтов на JS',
+    link: 'https://excel-krbln.web.app/',
+    imageSrc: 'img/portfolio/min/site-builder.jpg',
+    alt: 'Мини-конструктор сайтов на JS'
+  },
+  {
+    title: 'Приложение «Задай мне вопрос» на JavaScript + MUI + Firebase',
+    link: 'https://excel-krbln.web.app/',
+    imageSrc: 'img/portfolio/min/question.jpg',
+    alt: 'Приложение «Задай мне вопрос» на JavaScript + MUI + Firebase'
+  },
+  {
+    title: 'Плагин на JavaScript. Загрузка изображений на сервер Firebase',
+    link: 'https://plugin-upload-krbln.web.app/',
+    imageSrc: 'img/portfolio/min/plugin-upload.jpg',
+    alt: 'Плагин на JavaScript. Загрузка изображений на сервер Firebase'
+  },
+  {
+    title: 'Верстка интернет-магазина. GoldFish',
+    link: 'http://o92141m5.beget.tech/goldFish/',
+    imageSrc: 'img/portfolio/min/goldfish.jpg',
+    alt: 'Верстка интернет-магазина. GoldFish'
+  },
+  {
+    title: 'Верстка лендинга с посадкой на wordpress. Furniture shop',
+    link: 'http://o92141m5.beget.tech/FurnitureShop/',
+    imageSrc: 'img/portfolio/min/furniture-shop.jpg',
+    alt: 'Верстка лендинга с посадкой на wordpress. Furniture shop'
+  },
+  {
+    title: 'Викторина по истории (quiz-history)',
+    link: 'https://leonid-korobkov.github.io/quiz-history/',
+    imageSrc: 'img/portfolio/min/quiz-history.jpg',
+    alt: 'Викторина по истории (quiz-history)'
+  },
+  {
+    title: 'Отскакивающие мячики (bounceBalls)',
+    link: 'https://leonid-korobkov.github.io/bounceBalls/',
+    imageSrc: 'img/portfolio/min/bounce-balls.jpg',
+    alt: 'Отскакивающие мячики (bounceBalls)'
+  },
+  {
+    title: 'Генератор цветов (RandomColors)',
+    link: 'https://leonid-korobkov.github.io/RandomColors/',
+    imageSrc: 'img/portfolio/min/random-color.jpg',
+    alt: 'Генератор цветов (RandomColors)'
+  },
+  {
+    title: 'Игра "Змейка"(SnakeGame)',
+    link: 'https://leonid-korobkov.github.io/game-snake/',
+    imageSrc: 'img/portfolio/min/snake.jpg',
+    alt: 'Игра "Змейка"(SnakeGame)'
+  },
+  {
+    title: 'Игра "Виселица"(hangman-game)',
+    link: 'https://leonid-korobkov.github.io/hangman-game/',
+    imageSrc: 'img/portfolio/min/hangman.jpg',
+    alt: 'Игра "Виселица"(hangman-game)'
+  },
+  {
+    title: 'Игра на прицеливание (aim-game)',
+    link: 'https://leonid-korobkov.github.io/aim-game/',
+    imageSrc: 'img/portfolio/min/aim-game.jpg',
+    alt: 'Игра на прицеливание (aim-game)'
+  },
+  {
+    title: 'Мини-клон Trello. HTML, CSS, JS.',
+    link: 'https://leonid-korobkov.github.io/Drag-n-Drop/',
+    imageSrc: 'img/portfolio/min/drag-drop.jpg',
+    alt: 'Мини-клон Trello. HTML, CSS, JS.'
+  },
+  {
+    title: 'Доска для рисования (board)',
+    link: 'https://leonid-korobkov.github.io/board/',
+    imageSrc: 'img/portfolio/min/board.jpg',
+    alt: 'Доска для рисования (board)'
+  },
+  {
+    title: 'Слайдер (slider)',
+    link: 'https://leonid-korobkov.github.io/slider/',
+    imageSrc: 'img/portfolio/min/slider.jpg',
+    alt: 'Слайдер (slider)'
+  },
+  {
+    title: 'Карточки (cards)',
+    link: 'https://leonid-korobkov.github.io/cards/',
+    imageSrc: 'img/portfolio/min/cards.jpg',
+    alt: 'Карточки (cards)'
+  },
+  {
+    title: 'Верстка сайта productivity',
+    link: 'https://leonid-korobkov.github.io/productivity/',
+    imageSrc: 'img/portfolio/min/productivity.jpg',
+    alt: 'Верстка сайта productivity (productivity)'
+  },
+  {
+    title: 'Верстка сайта Mitravel',
+    link: 'https://leonid-korobkov.github.io/Mitravel/',
+    imageSrc: 'img/portfolio/min/mitravel.jpg',
+    alt: 'Верстка сайта Mitravel (Mitravel)'
+  },
+  {
+    title: 'Верстка сайта ActiveBox',
+    link: 'https://leonid-korobkov.github.io/ActiveBox/',
+    imageSrc: 'img/portfolio/min/activebox.jpg',
+    alt: 'Верстка сайта ActiveBox (ActiveBox)'
+  }
+]
